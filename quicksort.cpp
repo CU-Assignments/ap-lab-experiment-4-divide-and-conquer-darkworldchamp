2f609d245136b65c10d6233d2753df30de08b2ed
Nikhil sharma <140893545+darkworldchamp@users.noreply.github.com>
2025-02-19 23:19:51 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,21 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int quicksort(int arr[],int l,int h){
-int p= partition(arr,l,h);
-quicksort(arr,l,p-1);
-quicksort(arr,p,h);
-}
-int partition(int arr[],int l ,int h){
-    int mid= (l+h)/2;
+
+int partition(int arr[], int l, int h) {
+    int mid = (l + h) / 2;
     int pivot = arr[mid];
-    int i=l-1;
-    for(int j=l;j<h;j++){
-        if(arr[j]<pivot){
+    swap(arr[mid], arr[h]); 
+    int i = l - 1;
+    for (int j = l; j < h; j++) {
+        if (arr[j] < pivot) {
             i++;
-            swap(arr[i],arr[j]);
+            swap(arr[i], arr[j]);
         }
     }
-    swap(arr[i+1],pivot);
-    return i+1;
+    swap(arr[i + 1], arr[h]); 
+    return i + 1;
 }
 
+void quicksort(int arr[], int l, int h) {
+    if (l < h) {
+        int p = partition(arr, l, h);
+        quicksort(arr, l, p - 1);
+        quicksort(arr, p + 1, h);
+    }
+}
+
+int main() {
+    int arr[] = {4, 2, 1, 6, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quicksort(arr, 0, n - 1); 
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+    return 0;
+}
